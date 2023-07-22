0ef8740555e2dc275edae9dd2cd51ede7ba64fa7
Bdynjon <dmitrydust2704@gmail.com>
2023-07-22 13:20:11 +0300

    finished test: test_guest_can_add_product_to_basket

--- pages/locators.py
+++ pages/locators.py
@@ -12,3 +12,7 @@ class LoginPageLocators:
 
 class ProductPageLocators:
     ADD_TO_BASKET_BUTTON = (By.CSS_SELECTOR, "button.btn-add-to-basket")
+    PRODUCT_NAME_RESULT = (By.CSS_SELECTOR, "div[id='messages'] div:nth-child(1) strong")
+    PRODUCT_COST_RESULT = (By.CSS_SELECTOR, "div[id='messages'] div:nth-child(3) strong")
+    PRODUCT_NAME = (By.CSS_SELECTOR, "div.product_main > h1")
+    PRODUCT_COST = (By.CSS_SELECTOR, "div.product_main > p.price_color")
--- pages/product_page.py
+++ pages/product_page.py
@@ -9,3 +9,13 @@ class ProductPage(BasePage):
     def add_to_basket(self):
         add_to_basket_button = self._browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON)
         add_to_basket_button.click()
+
+    def should_be_correct_product_name(self):
+        product_name = self._browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+        product_name_result = self._browser.find_element(*ProductPageLocators.PRODUCT_NAME_RESULT).text
+        assert product_name == product_name_result, "Wrong product was bought"
+
+    def should_be_correct_product_cost(self):
+        product_cost = self._browser.find_element(*ProductPageLocators.PRODUCT_COST).text
+        product_cost_result = self._browser.find_element(*ProductPageLocators.PRODUCT_COST_RESULT).text
+        assert product_cost == product_cost_result, "Wrong product cost was calculated"
