842c4623e6e50180544027b73f184383e3635d65
Bdynjon <dmitrydust2704@gmail.com>
2023-07-22 18:12:26 +0300

    add test: test_guest_cant_see_product_in_basket_opened_from_product_page, mark tests for review, deleted __init__.py

--- test_product_page.py
+++ test_product_page.py
@@ -21,6 +21,7 @@ class TestUserAddToBasketFromProductPage:
         page.open()
         page.should_not_be_success_message()
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         url = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
         page = ProductPage(browser, url)
@@ -38,12 +39,17 @@ def test_guest_cant_see_success_message(browser):
     page.should_not_be_success_message()
 
 
-def test_guest_can_add_product_to_basket(browser):
-    url = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=NewYear"
+N = list(range(10))
+N[7] = pytest.param("7", marks=pytest.mark.xfail)
+
+
+@pytest.mark.need_review
+@pytest.mark.parametrize('n', N)
+def test_guest_can_add_product_to_basket(browser, n):
+    url = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=promo{n}"
     page = ProductPage(browser, url)
     page.open()
     page.add_to_basket()
-    page.solve_quiz_and_get_code()
 
     page.should_be_correct_product_name()
     page.should_be_correct_product_cost()
@@ -54,7 +60,6 @@ def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     page = ProductPage(browser, url)
     page.open()
     page.add_to_basket()
-    page.solve_quiz_and_get_code()
 
     page.should_not_be_success_message()
 
@@ -64,7 +69,6 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page = ProductPage(browser, url)
     page.open()
     page.add_to_basket()
-    page.solve_quiz_and_get_code()
 
     page.should_success_message_disappeared()
 
@@ -79,6 +83,7 @@ def test_guest_should_see_login_link_on_product_page(browser):
     login_page.should_be_login_page()
 
 
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     url = "http://selenium1py.pythonanywhere.com/"
     page = ProductPage(browser, url)
@@ -87,3 +92,15 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
 
     basket_page = BasketPage(browser, browser.current_url)
     basket_page.should_be_empty_basket()
+
+
+@pytest.mark.need_review
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    url = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=NewYear"
+    page = ProductPage(browser, url)
+    page.open()
+    page.go_to_login_page()
+
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.open()
+    login_page.should_be_login_page()
