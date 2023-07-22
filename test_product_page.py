6c3fe3809e2f5aa5d843c2b4f133893525f0ad16
Bdynjon <dmitrydust2704@gmail.com>
2023-07-22 15:00:31 +0300

    add test: test_guest_should_see_login_link_on_product_page to test_product_page

--- test_product_page.py
+++ test_product_page.py
@@ -42,3 +42,10 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.solve_quiz_and_get_code()
 
     page.should_success_message_disappeared()
+
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
