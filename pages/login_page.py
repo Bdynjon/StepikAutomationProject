49d606a89e713222b1077392f1145fa44277e10a
Bdynjon <dmitrydust2704@gmail.com>
2023-07-22 12:09:28 +0300

    add: go to login page

--- pages/login_page.py
+++ pages/login_page.py
@@ -15,3 +15,8 @@ class LoginPage(BasePage):
 
     def should_be_register_form(self):
         assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not present"
+
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
