fa2ef4946a76e43174ac0befb474da0ba51d05b8
Bdynjon <dmitrydust2704@gmail.com>
2023-07-22 14:10:21 +0300

    parametrize: test_quest_can_add_product_to_busket

--- test_main_page.py
+++ test_main_page.py
@@ -1,28 +1,35 @@
+import pytest
+
 from pages.main_page import MainPage
 from pages.login_page import LoginPage
 from pages.product_page import ProductPage
 
 
-# def test_guest_can_go_to_login_page(browser):
-#     url = "http://selenium1py.pythonanywhere.com/"
-#     page = MainPage(browser, url)
-#     page.open()
-#     page.go_to_login_page()
-#
-#     login_page = LoginPage(browser, browser.current_url)
-#     login_page.open()
-#     login_page.should_be_login_page()
-#
-#
-# def test_guest_should_see_login_link(browser):
-#     url = "http://selenium1py.pythonanywhere.com/"
-#     page = MainPage(browser, url)
-#     page.open()
-#     page.should_be_login_link()
+def test_guest_can_go_to_login_page(browser):
+    url = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, url)
+    page.open()
+    page.go_to_login_page()
+
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.open()
+    login_page.should_be_login_page()
 
 
-def test_guest_can_add_product_to_basket(browser):
-    url = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+def test_guest_should_see_login_link(browser):
+    url = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, url)
+    page.open()
+    page.should_be_login_link()
+
+
+N = list(range(10))
+N[7] = pytest.param(7, marks=pytest.mark.xfail)
+
+
+@pytest.mark.parametrize('n', N)
+def test_guest_can_add_product_to_basket(browser, n):
+    url = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{n}"
     page = ProductPage(browser, url)
     page.open()
     page.add_to_basket()
