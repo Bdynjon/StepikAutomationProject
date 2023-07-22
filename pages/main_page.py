52fad6c4f0f48e077bdde4b7f6046958fa5e0ec0
Bdynjon <dmitrydust2704@gmail.com>
2023-07-22 11:30:18 +0300

    add test: test_guest_should_see_login_link in test_main_page.py, add method: should_be_login_link

--- pages/main_page.py
+++ pages/main_page.py
@@ -10,3 +10,6 @@ class MainPage(BasePage):
     def go_to_login_page(self):
         login_link = self._browser.find_element(By.CSS_SELECTOR, "#login_link")
         login_link.click()
+
+    def should_be_login_link(self):
+        self._browser.find_element(By.CSS_SELECTOR, "#login_link_invalid")
