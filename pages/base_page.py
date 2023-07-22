ede65ad70b1307d3a37b100f66cec813462bba5e
Bdynjon <dmitrydust2704@gmail.com>
2023-07-22 14:27:15 +0300

    add methods in BasePage: is_not_element_present and is_disappeared

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,4 +1,7 @@
 from selenium.common.exceptions import NoSuchElementException, NoAlertPresentException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support import expected_conditions as EC
 import math
 
 
@@ -17,6 +20,23 @@ class BasePage:
         except NoSuchElementException:
             return False
         return True
+
+    def is_disappeared(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self._browser, timeout, 1, TimeoutException). \
+                until_not(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return False
+
+        return True
+
+    def is_not_element_present(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self._browser, timeout).until(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return True
+
+        return False
 
     def solve_quiz_and_get_code(self):
         alert = self._browser.switch_to.alert
